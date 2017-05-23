ad082e7e0105bbc835cfabe5a418c925c6b1cef7
feikoritsema <feiko.ritsema@hotmail.com>
2017-05-23 11:08:34 +0200

    IpDialog now  checks the validity of the IP address.

--- Hestia/app/src/main/java/hestia/UI/IpDialog.java
+++ Hestia/app/src/main/java/hestia/UI/IpDialog.java
@@ -1,16 +1,15 @@
 package hestia.UI;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.rugged.application.hestia.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import hestia.backend.NetworkHandler;
 import hestia.backend.Cache;
@@ -25,6 +24,15 @@ public class IpDialog extends HestiaDialog{
     private String ip;
     private NetworkHandler networkHandler;
     private Cache cache;
+    private Pattern pattern;
+    private Matcher matcher;
+    private static final String ipSetTo = "Ip address set to : ";
+    private static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
 
     public IpDialog(Activity activity) {
         super(activity, R.layout.ip_dialog, "Set IP");
@@ -48,16 +56,23 @@ public class IpDialog extends HestiaDialog{
     @Override
     void pressConfirm() {
         ip = ipField.getText().toString();
-        Toast.makeText(getContext(), "ip is: " + ip, Toast.LENGTH_SHORT).show();
-        if(ip!=null) {
+        if(checkIp(ip)) {
             cache.setIp(ip);
             networkHandler.updateDevices();
-            Toast.makeText(getContext(),R.string.ipSetTo + cache.getIp() + ":"
+            Toast.makeText(getContext(),ipSetTo + cache.getIp() + ":"
                     + cache.getPort(),Toast.LENGTH_SHORT).show();
 
             //TODO give correct response from server after changing ip
             Toast.makeText(getContext(), "Server returned message: + serverMessage",
                     Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), R.string.invalidIp, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private boolean checkIp(String ip){
+        pattern = Pattern.compile(IPADDRESS_PATTERN);
+        matcher = pattern.matcher(ip);
+        return matcher.matches();
     }
 }
