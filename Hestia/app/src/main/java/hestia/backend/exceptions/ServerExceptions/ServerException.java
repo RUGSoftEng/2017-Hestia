a831f7b4bf1087ac434509d3d900f268be28c682
ghostflash101 <chrisausema@hotmail.com>
2017-05-28 16:24:11 +0200

    removed ServerFaultException, made ServerException a abstract class

--- Hestia/app/src/main/java/hestia/backend/exceptions/ServerExceptions/ServerException.java
+++ Hestia/app/src/main/java/hestia/backend/exceptions/ServerExceptions/ServerException.java
@@ -1,11 +1,14 @@
 package hestia.backend.exceptions.ServerExceptions;
 
 
-public class ServerException extends Exception {
+public abstract class ServerException extends Exception {
     String message;
 
     public ServerException(String message){
         this.message=message;
     }
 
+    public String getMessage(){
+        return message;
+    }
 }
