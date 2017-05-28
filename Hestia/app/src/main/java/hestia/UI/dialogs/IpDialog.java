aa70bee5bcd8aad6da215176c2fb8dd3826f3fc5
feikoritsema <feiko.ritsema@hotmail.com>
2017-05-28 13:12:54 +0200

    Regex for Ip check

--- Hestia/app/src/main/java/hestia/UI/dialogs/IpDialog.java
+++ Hestia/app/src/main/java/hestia/UI/dialogs/IpDialog.java
@@ -2,11 +2,16 @@ package hestia.UI.dialogs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.rugged.application.hestia.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import hestia.backend.ServerCollectionsInteractor;
 
 /**
@@ -16,8 +21,15 @@ import hestia.backend.ServerCollectionsInteractor;
 
 public class IpDialog extends HestiaDialog {
     private EditText ipField;
-    private String ip;
     private ServerCollectionsInteractor serverCollectionsInteractor;
+    private Pattern pattern;
+    private Matcher matcher;
+    private static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+    private final String INCORRECT_IP = "The IP address should be between 0.0.0.0 and 255.255.255.255";
 
     public IpDialog(Activity activity, ServerCollectionsInteractor serverCollectionsInteractor) {
         super(activity, R.layout.ip_dialog, "Set IP");
@@ -27,9 +39,8 @@ public class IpDialog extends HestiaDialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         ipField = (EditText) findViewById(R.id.ip);
-        if (this.serverCollectionsInteractor.getHandler().getIp() != null) {
+        if (this.serverCollectionsInteractor.getHandler().getIp()!=null){
             ipField.setText(this.serverCollectionsInteractor.getHandler().getIp());
         }
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
@@ -40,8 +51,9 @@ public class IpDialog extends HestiaDialog {
 
     @Override
     void pressConfirm() {
-        ip = ipField.getText().toString();
-        if(ip!=null) {
+        String ip = ipField.getText().toString();
+        Log.d("IPDIALOG",ip);
+        if(ip!=null&&checkIp(ip)) {
             serverCollectionsInteractor.getHandler().setIp(ip);
             // TODO refresh layout
             Toast.makeText(getContext(),R.string.ipSetTo + serverCollectionsInteractor.getHandler().getIp() + ":"
@@ -50,6 +62,14 @@ public class IpDialog extends HestiaDialog {
             //TODO give correct response from server after changing ip
             Toast.makeText(getContext(), "Server returned message: + serverMessage",
                     Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), INCORRECT_IP,Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private boolean checkIp(String ip) {
+        pattern = Pattern.compile(IPADDRESS_PATTERN);
+        matcher = pattern.matcher(ip);
+        return matcher.matches();
     }
 }
